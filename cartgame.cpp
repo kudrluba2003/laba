d09ef179736c6895583c707f038eea66c7584993
shadrinNk <127694291+shadrinNk@users.noreply.github.com>
2023-03-13 00:35:28 +0300

    laba
    laba

---
+++ cartgame.cpp
@@ -0,0 +1,24 @@
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 10;
+    queue<int> a, b;
+    for (int i = 0, x; i < N / 2; ++i) cin >> x, a.push(x);
+    for (int i = 0, x; i < N / 2; ++i) cin >> x, b.push(x);
+    int moves = 0;
+    for (; !a.empty() && !b.empty() && moves <= 200000; ++moves)
+    {
+        int ac = a.front(), bc = b.front();
+        a.pop(); b.pop();
+        queue<int>* win = (ac == 0 && bc == N - 1) ? &a :
+            (bc == 0 && ac == N - 1) ? &b :
+            (ac > bc) ? &a : &b;
+        win->push(ac); win->push(bc);
+    }
+    if (a.empty()) cout << "second " << moves << endl;
+    else if (b.empty()) cout << "first " << moves << endl;
+    else cout << "botva\n";
+}
---
+++ different.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    std::map<int, bool> arr;
+
+    int n = 0;
+    int x;
+
+    std::cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> x;
+        arr[x] = true;
+    }
+
+    std::cout << "\n\n" << arr.size();
+
+    system("pause");
+    return 0;
+}
